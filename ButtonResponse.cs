b8a4b69a154f7d85af403caf4c9be1672be32355
shaved-ice <helenps.chang@gmail.com>
2025-06-13 14:18:34 +0100

    added more colours
    Used the new colours to indicate to the user the status of the program e.g recording, waiting etc.

--- ButtonResponse.cs
+++ ButtonResponse.cs
@@ -8,32 +8,58 @@ using System.Text.Json;
 public class ButtonResponse : MonoBehaviour
 {
     Leap.Controller player;
-    public GameObject indicator;
+    public GameObject indicator; //place your indicator object here in unity. It simply changes colour to indicate to the player what is happening
+    public Color readyColour = Color.green; //this allows users to change these colours to account for colorblindess etc. 
+    public Color waitColour = new Color(0.6f, 0.6f, 0.6f);
+    public Color recColour = Color.red;
+    public int secondsBeforeRecording = 5; //time before program starts recording
+    public int secondsToRecord = 5; 
+    private bool recTime = false;
+    private bool first = false;
+    private Material m;
     // Start is called before the first frame update
     void Start()
     {
-        Material m = indicator.GetComponent<MeshRenderer>().material;
+        m = indicator.GetComponent<MeshRenderer>().material;
         player = new Leap.Controller();
-        m.color = Color.red;
+        m.color = readyColour;
     }
 
     public void ButtonPress()
     {
-        Frame f = player.Frame(); 
-        for (int i = 0; i < f.Hands.Count; i++) //loop through every hand in the frame
-        {
-            for (int j = 0; j < f.Hands[i].fingers.Length; j++) //in the hand, go through every finger
-            {
-                if (f.Hands[i].fingers[j] != null)
-                {
-                    for (int k = 0; k < f.Hands[i].fingers[j].bones.Length; k++) //in our finger, go through every bone
-                    {
-                        Debug.Log(f.Hands[i].fingers[j].bones[k].ToString() + " on hand: " + i + " on finger: " + j);
-                    }
-                }
+        Frame f = player.Frame();
+        m.color = waitColour;
+        Invoke("delayCall", secondsBeforeRecording);
+        //for (int i = 0; i < f.Hands.Count; i++) //loop through every hand in the frame
+        //{
+        //    for (int j = 0; j < f.Hands[i].fingers.Length; j++) //in the hand, go through every finger
+        //    {
+        //        if (f.Hands[i].fingers[j] != null)
+        //        {
+        //            for (int k = 0; k < f.Hands[i].fingers[j].bones.Length; k++) //in our finger, go through every bone
+        //            {
+        //                Debug.Log(f.Hands[i].fingers[j].bones[k].ToString() + " on hand: " + i + " on finger: " + j);
+        //            }
+        //        }
 
-            }
-        }
+        //    }
+        //}
     }
 
+    private void delayCall()
+    {
+        recTime = true; 
+    }
+
+    void Update()
+    {
+        if (recTime && !first)
+        {
+            m.color = recColour;
+        }
+        else if (recTime)
+        {
+
+        }
+    }
 }
