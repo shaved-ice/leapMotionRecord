ee2e7c844fe511e17cfee505e5897bc52014e412
shaved-ice <helenps.chang@gmail.com>
2025-07-15 01:48:13 +0100

    started creation of replay system.
    First attempt at transformation + rotation simply by giving the function a vector3 position / quarternion rotation

--- Deserialize.cs
+++ Deserialize.cs
@@ -11,30 +11,19 @@ public class Deserialize : MonoBehaviour
 
     public string directoryPath;
     public string fileName;
-    public GameObject Indicator;
-    public Color readyColour = Color.green; //this allows users to change these colours to account for colorblindess etc. 
-    public Color waitColour = new Color(0.6f, 0.6f, 0.6f);
-    private Material m; 
+    public GameObject handModelLeft;
+    public GameObject handModelRight;
+    private FrameJson serialized;
+    private int frameNum = 0;
 
+    // I believe the three finger bones from palm to tip for each of the fingers of this model is the proximal, intermediate and distal 
+    // transforming finger tips seems to do nothing so I have left them out.
     // Start is called before the first frame update
     void Start()
     {
-        m = Indicator.GetComponent<MeshRenderer>().material;
-        m.color = readyColour;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void ButtonPress()
-    {
-        m.color = waitColour;
         Type t = typeof(FrameJson);
-        JsonSerializer j = new JsonSerializer(); 
-        FrameJson serialized;
+        JsonSerializer j = new JsonSerializer();
+        serialized = new FrameJson();
         byte[] arr = new byte[32];
         UTF8Encoding ut = new UTF8Encoding();
         string filePath = directoryPath + "\\" + fileName + ".txt";
@@ -49,13 +38,32 @@ public class Deserialize : MonoBehaviour
                 using (TextReader sr = new StreamReader(fs))
                 {
                     //serial = sr.ReadLine();
-                    serialized = (FrameJson) j.Deserialize(sr, t);
+                    serialized = (FrameJson)j.Deserialize(sr, t);
                 }
             }
-            Debug.Log(serialized.jsonList[0].handList[0].palmPos);
+            //Debug.Log(serialized.jsonList[0].handList[0].palmPos);
             //Debug.Log(serialized.JsonList[1]);
         }
-        //m.color = readyColour;
+
+        ////simple transform code to test finger movement
+        //HandBreakdown Hand = serialized.jsonList[0].handList[0];
+        //GameObject finger;
+        //GameObject palm;
+        //if (Hand.isLeft)
+        //{
+        //    palm = handModelLeft.transform.GetChild(0).GetChild(0).GetChild(0).gameObject;
+        //    finger = palm.transform.GetChild(1).gameObject;
+        //}
+        //else
+        //{
+        //    palm = handModelLeft.transform.GetChild(0).GetChild(0).GetChild(0).gameObject;
+        //    finger = palm.transform.GetChild(1).gameObject;
+        //}
+
+        //Debug.Log(handModelLeft.transform.GetChild(0).GetChild(0).gameObject); //prints wrists
+        //finger.transform.TransformPoint(Vector3.forward + Vector3.forward);
+        //finger.transform.GetChild(0).TransformPoint(Vector3.forward);
+
 
 
         //buffered version attempt:
@@ -83,6 +91,48 @@ public class Deserialize : MonoBehaviour
         //}
     }
 
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    TransformHand(serialized.jsonList[0].handList[0]);
+    //    TransformHand(serialized.jsonList[0].handList[1]);
+    //    frameNum = (frameNum + 1) % serialized.jsonList.Count; //increment the frameNum every time but don't go over the length of the framelist
+    //}
+
+    public void TransformHand(HandBreakdown b)
+    {
+        GameObject hand;
+        if (b.isLeft)
+        {
+            hand = handModelLeft.transform.GetChild(0).gameObject;
+        }
+        else
+        {
+            hand = handModelRight.transform.GetChild(0).gameObject;
+        }
+
+    }
+
+    public void moveToCoords(GameObject g, Vector3 v)
+    {
+        Vector3 resv;
+        Quaternion resq;
+        g.transform.GetPositionAndRotation(out resv, out resq); //this puts the position and rotation in our variables.
+        g.transform.Translate((resv.x -  v.x), (resv.y - v.y), (resv.z - v.z), Space.World);
+    }
+
+    public void rotateQ(GameObject g, Quaternion q)
+    {
+        Vector3 resv;
+        Quaternion resq;
+        Quaternion inverse;
+        g.transform.GetPositionAndRotation(out resv, out resq);
+        inverse = Quaternion.Inverse(resq); //by applying the inverse rotation to the current rotation I hope to fully reset the object's rotation.
+        g.transform.Rotate(inverse.eulerAngles, Space.World); //now we can apply our new rotation
+        g.transform.Rotate(q.eulerAngles, Space.World);
+
+    }
+
     //including the class structure so I can access data after deserialization.
     public class FrameJson
     {
@@ -96,7 +146,7 @@ public class Deserialize : MonoBehaviour
         {
             jsonList.Add(f);
         }
-    }
+    } 
 
     public class FrameBreakdown
     {
