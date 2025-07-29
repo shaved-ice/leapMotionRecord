b270bf6c3b750e839e00c12f87106c6c8b806e2b
shaved-ice <helenps.chang@gmail.com>
2025-07-30 00:35:42 +0100

    Create ScrubBarScript.cs
    Initial version of the script for my scrub bar prefab.
    This version responds to calls from the Deserializer.cs Script to know when to update it's display.

---
+++ ScrubBarScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrubBarScript : MonoBehaviour
+{
+    //public GameObject deserializeHolder; //GameObject holding the Deserialize script
+    private Deserialize deserializer;
+    private GameObject scrubber;
+    private GameObject Pointer;
+    private float ratio;
+    private float startOfBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //d = deserializeHolder.GetComponent<Deserialize>();
+        deserializer = FindFirstObjectByType<Deserialize>();
+        //this.gameObject is the ScrubBar parent object as this script will be placed on that object.
+        Pointer = this.gameObject.transform.GetChild(1).gameObject; //this starts off at position 0 in the prefab
+        startOfBar = Pointer.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void initialiseScrubBar()
+    {
+        ratio = 1.0f / (deserializer.getMaxSize() - 1);
+    }
+
+    public void updateScrubBar()
+    {
+        Vector3 oldPos = Pointer.transform.position;
+        Pointer.transform.position = new Vector3(oldPos.x, oldPos.y, startOfBar + (ratio * deserializer.getFrameNum()));
+    }
+}
