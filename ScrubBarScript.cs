1c1bd4e492182a94f70c395b1b130dc4109c9e37
shaved-ice <helenps.chang@gmail.com>
2025-07-31 02:04:09 +0100

    moved code from deserializer to this
    I moved the initialisation call from the deserializer script to this script and also moved the duty of checking if the scrubBar needs to update to from the deserializer script to this script in order to decrease overlap.

--- ScrubBarScript.cs
+++ ScrubBarScript.cs
@@ -10,6 +10,7 @@ public class ScrubBarScript : MonoBehaviour
     private GameObject Pointer;
     private float ratio;
     private float startOfBar;
+    private int frameNum = 0; //initialise to the start
     // Start is called before the first frame update
     void Start()
     {
@@ -18,22 +19,23 @@ public class ScrubBarScript : MonoBehaviour
         //this.gameObject is the ScrubBar parent object as this script will be placed on that object.
         Pointer = this.gameObject.transform.GetChild(1).gameObject; //this starts off at position 0 in the prefab
         startOfBar = Pointer.transform.position.z;
+        ratio = 1.0f / (deserializer.getMaxSize() - 1);
+        updateScrubBar();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!(deserializer.frameDiff(frameNum)))
+        {
+            updateScrubBar();
+        }
     }
 
-    public void initialiseScrubBar()
-    {
-        ratio = 1.0f / (deserializer.getMaxSize() - 1);
-    }
-
-    public void updateScrubBar()
+    private void updateScrubBar()
     {
         Vector3 oldPos = Pointer.transform.position;
-        Pointer.transform.position = new Vector3(oldPos.x, oldPos.y, startOfBar + (ratio * deserializer.getFrameNum()));
+        frameNum = deserializer.FrameNum;
+        Pointer.transform.position = new Vector3(oldPos.x, oldPos.y, startOfBar + (ratio * frameNum));
     }
 }
