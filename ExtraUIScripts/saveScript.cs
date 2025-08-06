9c83acaad910504a5b46036e4922e45627538f78
shaved-ice <helenps.chang@gmail.com>
2025-08-07 00:50:56 +0100

    Update saveScript.cs
    added ability to delete a save file,
    added status text to let the user to know what's happening
    e.g no file to load so nothing happened, file saved, file deleted etc. 
    clean up and added comments to code

--- ExtraUIScripts/saveScript.cs
+++ ExtraUIScripts/saveScript.cs
@@ -6,19 +6,18 @@ using TMPro;
 public class saveScript : MonoBehaviour
 {
     public TMP_Text[] fileHolder = new TMP_Text[3]; //unfortunately this allows the user to change the size of the array. I only account for the first 3 and this code will likely break if there are less than 2
-    //public TMP_Text File1; //these are the text boxes on the cubes. I use them to display my save files.
-    //public TMP_Text File2;
-    //public TMP_Text File3;
+    public TMP_Text status; //alerts the user that something has happened
     private int currentFile = 1; //starting with the file in the middle for selection purposes
     private string saveFileName = "";
     private Deserialize deserializer;
     private bool[] storedOrNot = new bool[3];
+    private Deserialize.FrameJson emptyFile = new Deserialize.FrameJson();
     private Deserialize.FrameJson[] storage = new Deserialize.FrameJson[3]; //this is my display array - they will be displayed in unity in clockwise order from the middle - starting from whoever's index is the currentFile.
 
     void Start()
     {
-        deserializer = FindFirstObjectByType<Deserialize>();
-        for (int i = 0; i < 3; i++)
+        deserializer = FindFirstObjectByType<Deserialize>(); //initialise deserialize script connection
+        for (int i = 0; i < 3; i++) //initialise the storedOrNot array
         {
             storedOrNot[i] = false;
         }
@@ -28,6 +27,7 @@ public class saveScript : MonoBehaviour
     {
         saveFileName = newFileName;
     }
+
     public void saveAFile()
     {
         storage[currentFile] = deserializer.getFile(); //save the file into our storage at the current index
@@ -40,6 +40,7 @@ public class saveScript : MonoBehaviour
             fileHolder[1].text = saveFileName;
         }
         storedOrNot[currentFile] = true;
+        status.text = "file saved!";
     }
 
     public void loadAFile()
@@ -47,23 +48,37 @@ public class saveScript : MonoBehaviour
         if (storedOrNot[currentFile])
         {
             deserializer.setFile(storage[currentFile]); //call the deserializer load file function to load the file
+            status.text = "file loaded!";
+        }
+        else
+        {
+            status.text = "file is empty!";
         }
     }
 
-    public void nextFile()
+    public void deleteASaveFile()
+    {
+        storage[currentFile] = emptyFile; //change it to take up less storage space
+        storedOrNot[currentFile] = false;
+        fileHolder[1].text = "Empty";
+        status.text = "file deleted!";
+    }
+
+    public void prevFile()
     {
         string[] textHolder = new string[3];
         for (int j = 0; j < 3; j++)  //save all the text so we don't lose any
         {
             textHolder[j] = fileHolder[j].text;
         }
-        currentFile = (currentFile - 1); //increment which file we're looking at
+        currentFile = (currentFile - 1); //decrement which file we're looking at
         if (currentFile == -1)
         {
             currentFile = 2;
         }
         //this is the new middle file
         for (int i = 0; i < 3; i++) //now just replace all the text with the text of the save file previous
+        //aka move all the text one over to the right
         {
             int oldSpace;
             if (i == 0)
@@ -78,7 +93,7 @@ public class saveScript : MonoBehaviour
         }
     }
 
-    public void prevFile()
+    public void nextFile()
     {
         string[] textHolder = new string[3];
         for (int j = 0; j < 3; j++)  //save all the text so we don't lose any
@@ -86,8 +101,8 @@ public class saveScript : MonoBehaviour
             textHolder[j] = fileHolder[j].text;
         }
         currentFile = (currentFile + 1) % 3; //increment which file we're looking at
-        //this is the new middle file
-        for (int i = 0; i < 3; i++) //now just replace all the text with the text of the save file previous
+        for (int i = 0; i < 3; i++) //now just replace all the text with the text of the save file after it
+        //aka move all the text one over to the left
         {
             int oldSpace;
             if (i == 2)
