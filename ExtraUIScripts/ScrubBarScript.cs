c1ec72f52c2e9cfd0fea259a2b797b0e59d741f8
shaved-ice <helenps.chang@gmail.com>
2025-08-07 00:47:42 +0100

    added the ability to reset for when a new file is loaded into ScrubBarScript.cs
    added ability to reset
    cleaned up code + added comments

--- ExtraUIScripts/ScrubBarScript.cs
+++ ExtraUIScripts/ScrubBarScript.cs
@@ -1,32 +1,37 @@
-using System.Collections;
+uusing System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ScrubBarScript : MonoBehaviour
 {
-    //public GameObject deserializeHolder; //GameObject holding the Deserialize script
     private Deserialize deserializer;
     private GameObject scrubber;
     private GameObject Pointer;
     private float ratio;
     private float startOfBar;
     private int frameNum = 0; //initialise to the start
+    private int maxSize;
+    private int fileNum;
     // Start is called before the first frame update
     void Start()
     {
-        //d = deserializeHolder.GetComponent<Deserialize>();
         deserializer = FindFirstObjectByType<Deserialize>();
         //this.gameObject is the ScrubBar parent object as this script will be placed on that object.
-        Pointer = this.gameObject.transform.GetChild(1).gameObject; //this starts off at position 0 in the prefab
+        Pointer = this.gameObject.transform.GetChild(1).gameObject; //this starts off at the start of the bar in my prefab
         startOfBar = Pointer.transform.position.z;
-        ratio = 1.0f / (deserializer.getMaxSize() - 1);
+        calculateRatio();
         updateScrubBar();
+        fileNum = deserializer.getFileNum();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!(deserializer.frameDiff(frameNum)))
+        if (fileNum != deserializer.getFileNum()) //file has changed so we need to change the ratio as we have a new maxSize
+        {
+            calculateRatio(); //don't update frameNum as it will update or stay as 0 next anyway
+        }
+        if (deserializer.frameDiff(frameNum))
         {
             updateScrubBar();
         }
@@ -36,6 +41,13 @@ public class ScrubBarScript : MonoBehaviour
     {
         Vector3 oldPos = Pointer.transform.position;
         frameNum = deserializer.FrameNum;
-        Pointer.transform.position = new Vector3(oldPos.x, oldPos.y, startOfBar + (ratio * frameNum));
+        Pointer.transform.position = new Vector3(oldPos.x, oldPos.y, startOfBar + (ratio * frameNum)); //calculate new pointer position based on old position and the new frameNum
+    }
+
+    private void calculateRatio()
+    {
+        maxSize = deserializer.getMaxSize();
+        ratio = 1.0f / (maxSize - 1);
     }
 }
+
