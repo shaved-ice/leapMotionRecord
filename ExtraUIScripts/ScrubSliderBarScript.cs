eed7622224dbac10ef07492cd66bb6baf12d165b
shaved-ice <helenps.chang@gmail.com>
2025-08-06 23:37:28 +0100

    cleaned up ScrubSliderBarScript.cs
    cleaned up the script and added comments.
    I also added functionality to reset the bar when a new file is being replayed e.g read/loaded in

--- ExtraUIScripts/ScrubSliderBarScript.cs
+++ ExtraUIScripts/ScrubSliderBarScript.cs
@@ -9,36 +9,30 @@ public class ScrubSliderBarScript : MonoBehaviour
     // Start is called before the first frame update
     private Deserialize deserializer;
     public Slider scrubSlider;
-    //private RectTransform rect;
     private float slidescript;
     private UnityEvent<SliderEventData> slideEvent;
     private float fNum = 0f;
     private bool selectBool = false; //let's us know if we're currently being selected or not.
-    //private UnityAction action;
+    private int fileNum;
 
     void Start()
     {
         deserializer = FindFirstObjectByType<Deserialize>();
         scrubSlider = FindFirstObjectByType<Slider>();
-        scrubSlider.MaxValue = (deserializer.getMaxSize() - 1); //this way we don't have to waste time calculating the percentage space we need to put the slider in
-        scrubSlider.Value = 0f; //always restart the bar to 0
-        //an attempt at adding the function to the change value UnityEvent without using the editor.
-        //slideEvent = scrubSlider.OnValueUpdated;
-        //action = action + functionForSlideUpdate;
-        //scrubSlider.OnValueUpdated.AddListener(action);
-
-        ////old code to change the width of the slider.
-        //rect = scrubSlider.GetComponent<RectTransform>();
-        //Vector2 newDelta = rect.sizeDelta;
-        //newDelta.x = 300; //I chose this number as it looked long enough to be able to accomodate a large range of video sizes but wasn't too big as to be hard to use.
-        //rect.sizeDelta = newDelta; 
-
+        initialiseSlider();
+        fileNum = deserializer.getFileNum();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ( (!(deserializer.frameDiff((int)fNum))) && !selectBool)
+        int newFileNum = deserializer.getFileNum();
+        if (fileNum != newFileNum)
+        {
+            fileNum = newFileNum; //new file read so we restart everything
+            initialiseSlider();
+        }
+        if ((deserializer.frameDiff((int)fNum)) && !selectBool) //if our frameNum is different from the one actually being replayed AND the user isn't currently selecting us then update the bar
         {
             fNum = (float)(deserializer.FrameNum);
             scrubSlider.Value = fNum;
@@ -46,13 +40,19 @@ public class ScrubSliderBarScript : MonoBehaviour
 
     }
 
+    public void initialiseSlider()
+    {
+        scrubSlider.MaxValue = (deserializer.getMaxSize() - 1); //this way we don't have to waste time calculating the percentage space we need to put the slider in
+        scrubSlider.Value = 0f; //always restart the bar to 0
+    }
+
     public void functionSelectUpdate()
     {
-        selectBool = true;
+        selectBool = true; //lets us know we are currently being selected so avoid updating the bar as not to hinder the user
     }
     public void functionUnselectUpdate()
     {
-        deserializer.FrameNum = (int)scrubSlider.Value;
+        deserializer.FrameNum = (int)scrubSlider.Value; //update deserializer with the new frame num
         selectBool = false;
     }
 }
