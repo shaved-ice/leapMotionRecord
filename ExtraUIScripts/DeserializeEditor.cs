bb84d955435811293f5f63695a87158bb1e8f27d
shaved-ice <helenps.chang@gmail.com>
2025-08-06 23:30:53 +0100

    added extra buttons to the Deserialize editor

--- ExtraUIScripts/DeserializeEditor.cs
+++ ExtraUIScripts/DeserializeEditor.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using UnityEditor;
 
 [CustomEditor(typeof(Deserialize))]
@@ -9,22 +6,36 @@ public class DeserializeEditor : Editor
     public override void OnInspectorGUI()
     {
         DrawDefaultInspector();
-        Deserialize s = (Deserialize)target;
-        if (GUILayout.Button("Play", GUILayout.Width(70)))
+        Deserialize s = (Deserialize)target;    
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("First", GUILayout.Width(70)))
         {
-            s.Play();
+            s.FirstFrame();
         }
-        if (GUILayout.Button("Pause", GUILayout.Width(70)))
+        if (GUILayout.Button("<<Prev", GUILayout.Width(70)))
         {
-            s.Pause();
+            s.PrevFrameLarge();
+        }
+        if (GUILayout.Button("Prev", GUILayout.Width(70)))
+        {
+            s.PrevFrame();
+        }
+        if (GUILayout.Button("Play/Pause", GUILayout.Width(90)))
+        {
+            s.PausePlay();
         }
         if (GUILayout.Button("Next", GUILayout.Width(70)))
         {
             s.NextFrame();
         }
+        if (GUILayout.Button("Next>>", GUILayout.Width(70)))
+        {
+            s.NextFrameLarge();
+        }
         if (GUILayout.Button("Last", GUILayout.Width(70)))
         {
             s.LastFrame();
         }
+        GUILayout.EndHorizontal();
     }
 }
