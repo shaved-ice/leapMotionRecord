4083352f62875441507b3a2e066e73dd1cbad7ed
shaved-ice <helenps.chang@gmail.com>
2025-08-08 04:02:56 +0100

    Updated ScrubBarScript.cs to conform to function naming conventions

--- ExtraUIScripts/ScrubBarScript.cs
+++ ExtraUIScripts/ScrubBarScript.cs
@@ -1,4 +1,4 @@
-uusing System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -19,35 +19,34 @@ public class ScrubBarScript : MonoBehaviour
         //this.gameObject is the ScrubBar parent object as this script will be placed on that object.
         Pointer = this.gameObject.transform.GetChild(1).gameObject; //this starts off at the start of the bar in my prefab
         startOfBar = Pointer.transform.position.z;
-        calculateRatio();
-        updateScrubBar();
-        fileNum = deserializer.getFileNum();
+        CalculateRatio();
+        UpdateScrubBar();
+        fileNum = deserializer.GetFileNum();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (fileNum != deserializer.getFileNum()) //file has changed so we need to change the ratio as we have a new maxSize
+        if (fileNum != deserializer.GetFileNum()) //file has changed so we need to change the ratio as we have a new maxSize
         {
-            calculateRatio(); //don't update frameNum as it will update or stay as 0 next anyway
+            CalculateRatio(); //don't update frameNum as it will update or stay as 0 next anyway
         }
-        if (deserializer.frameDiff(frameNum))
+        if (deserializer.FrameDiff(frameNum))
         {
-            updateScrubBar();
+            UpdateScrubBar();
         }
     }
 
-    private void updateScrubBar()
+    private void UpdateScrubBar()
     {
         Vector3 oldPos = Pointer.transform.position;
         frameNum = deserializer.FrameNum;
         Pointer.transform.position = new Vector3(oldPos.x, oldPos.y, startOfBar + (ratio * frameNum)); //calculate new pointer position based on old position and the new frameNum
     }
 
-    private void calculateRatio()
+    private void CalculateRatio()
     {
-        maxSize = deserializer.getMaxSize();
+        maxSize = deserializer.GetMaxSize();
         ratio = 1.0f / (maxSize - 1);
     }
 }
-
